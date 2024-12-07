aed07339b7e6a077cda4b3e164e9dae242e4356c
Lilyof1hevalley <nfa190506@gmail.com>
2024-12-07 17:53:47 +0700

    Multiplicantion function also a condition other than the option input

--- mainc_group8.c
+++ mainc_group8.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
+void multiply(int *a, int *b) {
+    printf("%d\n", (*a) * (*b));
+}
+
 int main(){
-    int a, b, i;
+    int a, b, i, n;
+	printf("==CALCULATOR==\n");
     printf("Please input (1 for addition, 2 for substraction, 3 for multiplication and 4 for division)\n");
     scanf("%d", &i);
 
     printf("Enter 2 integer number:");
     scanf("%d %d", &a, &b);
     if (i == 1){
-        printf("addition for %d and %d = %d\n", a, b, a+b);
+        printf("addition for %d and %d = %d", a, b, a+b);
     }
     else if (i==2){
-        printf("substraction for %d and %d = %d\n", a, b, a-b);
+        printf("substraction for %d and %d = %d", a, b, a-b);
     }
+    else if (i==3){
+		printf("Multiplication of %d and %d = ", a, b);
+		multiply(&a, &b);
+	}
+	else{
+		printf("Invalid input. Please select a valid operation.\n");
+	}
     return 0;
 }
