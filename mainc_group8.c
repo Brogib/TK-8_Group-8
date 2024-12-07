20a55ccd5d06ec386092890a667323a5d4970e14
Lilyof1hevalley <nfa190506@gmail.com>
2024-12-07 18:01:55 +0700

    Adding some while loop so that user dont have to run the program again after one time operation, also added some changes for aesthetic purposes

--- mainc_group8.c
+++ mainc_group8.c
@@ -12,28 +12,31 @@ void divide(int *c, int *d) {
 
 int main(){
     int a, b, i, n;
-	printf("==CALCULATOR==\n");
-    printf("Please input (1 for addition, 2 for substraction, 3 for multiplication and 4 for division)\n");
+	printf("==CALCULATOR==");
+	
+	while (n<=4){
+    printf("\nPlease input\n1 for addition\n2 for substraction\n3 for multiplication\n4 for division\n=Input you pick=: ");
     scanf("%d", &i);
-
-    printf("Enter 2 integer number:");
+    printf("Enter 2 integer number: ");
     scanf("%d %d", &a, &b);
-    if (i == 1){
-        printf("addition for %d and %d = %d", a, b, a+b);
-    }
-    else if (i==2){
-        printf("substraction for %d and %d = %d", a, b, a-b);
-    }
-    else if (i==3){
-		printf("Multiplication of %d and %d = ", a, b);
-		multiply(&a, &b);
-	}
-	else if (i==4){
-		printf("Division of %d and %d = ", a, b);
-		divide(&a, &b);
-	} 
-	else{
-		printf("Invalid input. Please select a valid operation.\n");
+    
+	    if (i == 1){
+	        printf("addition for %d and %d = %d\n", a, b, a+b);
+	    }
+	    else if (i==2){
+	        printf("substraction for %d and %d = %d\n", a, b, a-b);
+	    }
+	    else if (i==3){
+			printf("Multiplication of %d and %d = \n", a, b);
+			multiply(&a, &b);
+		}
+		else if (i==4){
+			printf("Division of %d and %d = \n", a, b);
+			divide(&a, &b);
+		} 
+		else{
+			printf("Invalid input. Please select a valid operation.\n");
+		}
 	}
     return 0;
 }
